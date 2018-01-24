be08f175085aec6f3c9f6df3d003ac868085df5c
Jake-L <ekaj@sympatico.ca>
2018-01-24 15:20:23 -0500

    added test cases

--- Client/Client.java
+++ Client/Client.java
@@ -10,7 +10,6 @@ public final class Client
 		int port = 5555;
 		
 		// Get the server address from the user
-		
 		String host = InetAddress.getLocalHost().getHostAddress();//"10.84.98.43";
 		
 		Socket socket = null;
@@ -44,10 +43,36 @@ public final class Client
 		{
 			fromServer = in.nextLine();
 			System.out.println("Server: " + fromServer);
+
+			System.out.println("Enter input to send to server, or press a number from 1-6 to send a premade message\n1 - SUBMIT book1\n2 - UPDATE book1\n3 - GET book1 & book2 by author\n4 - GET ALL\n5 - SUBMIT book2\n6 - REMOVE book1 & book2 by author");
 	
 			fromUser = keyboardIn.nextLine();
-			
-			if (fromUser != null) 
+
+			if (fromUser == "1")
+			{
+				out.println("SUBMIT\nISBN 9783161484100\nTITLE Modular Algorithms\nAUTHOR Gerhard\nPUBLISHER Mir");
+			}
+			else if (fromUser == "2")
+			{
+				out.println("UPDATE\nISBN 9783161484100\nYEAR 2004\nPUBLISHER Springer");
+			}
+			else if (fromUser == "3")
+			{
+				out.println("GET\nAUTHOR Gerhard");
+			}
+			else if (fromUser == "4")
+			{
+				out.println("GET\nALL");
+			}
+			else if (fromUser == "5")
+			{
+				out.println("SUBMIT\nISBN 9780785195368\nTITLE Daredevil\nAUTHOR Gerhard\nPUBLISHER Marvel");
+			}
+			else if (fromUser == "6")
+			{
+				out.println("REMOVE\nAUTHOR Gerhard");
+			}
+			else if (fromUser != null) 
 			{
 				System.out.println("Client: " + fromUser);
 				out.println(fromUser + "\ntestline2");
